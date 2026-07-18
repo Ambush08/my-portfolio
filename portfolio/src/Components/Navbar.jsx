3406198dfebb34262e12e7accf5d8920e10ecc23
ambush.dev <204449149+Ambush08@users.noreply.github.com>
2026-07-18 18:16:26 +0300

    Add logo to navbar component

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,8 +1,10 @@
-import menu from '../Backend/data';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="w-full py-8 shadow-lg">
+        <div className="max-w-6xl mx-auto flex items-center justify-between">Navbar</div>
+    </header>
   )
 }
 
