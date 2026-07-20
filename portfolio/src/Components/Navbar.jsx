831b8b37411756fc9f74944ccc5e199a60597fb5
ambush.dev <204449149+Ambush08@users.noreply.github.com>
2026-07-20 09:34:38 +0300

    Make navbar menu button interactive

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,9 +1,20 @@
-
+import {useState} from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="w-full p-8 shadow-lg">
-        <div className="max-w-6xl mx-auto flex items-center justify-between">Navbar</div>
+        <div className="max-w-6xl mx-auto flex items-center justify-between">
+            <div className="text-2xl font-extrabold lg:text-3xl">Ambush.<span className="text-fuchsia-900">Dev</span></div>
+            <button onClick={()=>{setIsOpen(!isOpen)}} className="text-2xl sm:hidden">
+                {
+                    isOpen ? <i class="fa-solid fa-x"></i> : <i class="fa-solid fa-bars"></i>
+                }
+            </button>
+        </div>
     </header>
   )
 }
