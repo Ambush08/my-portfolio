c4fac9518ee55050b82f58d6ad7033d5e8dfae05
ambush.dev <204449149+Ambush08@users.noreply.github.com>
2026-07-20 10:10:50 +0300

    Change logo color

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <header className="w-full p-8 shadow-lg">
         <div className="max-w-6xl mx-auto flex items-center justify-between">
-            <div className="text-2xl font-extrabold lg:text-3xl">Ambush.<span className="text-fuchsia-900">Dev</span></div>
+            <div className="text-2xl font-extrabold lg:text-3xl">Ambush.<span className="text-primary">Dev</span></div>
             <button onClick={()=>{setIsOpen(!isOpen)}} className="text-2xl sm:hidden">
                 {
                     isOpen ? <i class="fa-solid fa-x"></i> : <i class="fa-solid fa-bars"></i>
